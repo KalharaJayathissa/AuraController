74376811d4059803b6d776342132eab1b757220f
kalhara <kalharajay@gmail.com>
2025-07-13 01:39:58 +0530

    spring boot application initiation

---
+++ src/main/java/com/example/aurabck/AurabckApplication.java
@@ -0,0 +1,13 @@
+package com.example.aurabck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurabckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AurabckApplication.class, args);
+    }
+
+}

1acbe77952db36472d4e82fa9f0bf92f1eaed3df
kalhara <kalharajay@gmail.com>
2025-07-15 18:06:28 +0530

    component update

--- src/main/java/com/example/aurabck/module/Task.java
+++ src/main/java/com/example/aurabck/module/Task.java
@@ -1,6 +1,8 @@
 package com.example.aurabck.module;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //configure auto increment in mysql database
     private int id;
     private String module;
     private String task;
